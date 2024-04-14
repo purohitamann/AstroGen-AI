95fe904f155b7bdf6fd8d7878d6ca72cd26930c6
Aman Purohit <purohiam@sheridancollege.ca>
2024-04-14 13:37:36 -0400

    Initial Commit

---
+++ AstroGen-AI/AstroGen_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroGen_AIApp.swift
+//  AstroGen-AI
+//
+//  Created by Aman Purohit on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct AstroGen_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
